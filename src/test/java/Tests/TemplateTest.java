2936c5cc2e860273c5d3a870c274c54538031324
Kumar Raju <kumar.raj@neutrinotechlabs.com>
2022-01-30 06:02:54 +0000

    final code

--- src/test/java/Tests/TemplateTest.java
+++ src/test/java/Tests/TemplateTest.java
@@ -49,92 +49,95 @@ public class TemplateTest extends BasePage {
         TemplatePageObj.ClickCancelCreateTemplate();
         Thread.sleep(3000);
     }
-//    @Test(priority = 3)
-//    public void create_template_with_already_exists_name_no_chosen_file() throws Exception {
-//        //TC 20.3 - Create Template with Already Exists Name and Chosen file
-//        TemplatePageObj.ClickOnUploadTemplateBtn();
-//        Thread.sleep(2000);
-//        TemplatePageObj.ClickOnTemplateName(ReadProps.readAttr("TExistName"));
-//        Thread.sleep(2000);
-//        upload_file.sendKeys("C:\\Users\\kishor_ghadge\\IdeaProjects\\Template1.jpg");
-//        Thread.sleep(3000);
-//        TemplatePageObj.ClickCreateTemplate();
-//        Thread.sleep(5000);
-//        TemplatePageObj.ClickCancelCreateTemplate();
-//        Thread.sleep(3000);
-//    }
-//    @Test(priority = 4)
-//    public void create_template_with_valid_name_chosen_file() throws Exception {
-//        //TC 20.4 - Create template with Valid Name and chosen file.
-//        TemplatePageObj.ClickOnUploadTemplateBtn();
-//        Thread.sleep(2000);
-//        TemplatePageObj.ClickOnTemplateName(ReadProps.readAttr("TNameUnique"));
-//        Thread.sleep(2000);
-//        upload_file.sendKeys("C:\\Users\\kishor_ghadge\\IdeaProjects\\Template1.jpg");
-//        Thread.sleep(2000);
-//        TemplatePageObj.ClickCreateTemplate();
-//        Thread.sleep(6000);
-//        TemplatePageObj.ClickCancel();
-//        Thread.sleep(5000);
-//    }
-//    @Test(priority = 5)
-//    public void search_created_template() throws Exception {
-//        //TC 20.5 Search Created template.
-//        TemplatePageObj.ClickOnSearchTemplate(ReadProps.readAttr("TNameUnique"));
-//        Thread.sleep(2000);
-//        TemplatePageObj.ClickOnExpandTemplate();
-//        Thread.sleep(2000);
-//        TemplatePageObj.ClickOnTemplateDataInfo();
-//        Thread.sleep(4000);
-//    }
-//    @Test(priority = 6)
-//    public void zoomin() throws Exception {
-//        //TC 20.6 Zoom IN.
-//        TemplatePageObj.ClickOnZoomIn();
-//        Thread.sleep(2000);
-//        TemplatePageObj.ClickOnZoomIn();
-//        Thread.sleep(2000);
-//        JavascriptExecutor js = (JavascriptExecutor) driver;
-//        js.executeScript("window.scrollBy(0,10000)", "");
-//        Thread.sleep(2000);
-//    }
-//    @Test(priority = 7)
-//    public void zoomout() throws Exception {
-//        //TC 20.7 Zoom OUT.
-//        TemplatePageObj.ClickOnZoomOut();
-//        Thread.sleep(2000);
-//        TemplatePageObj.ClickOnZoomOut();
-//        Thread.sleep(2000);
-//        JavascriptExecutor js = (JavascriptExecutor) driver;
-//        js.executeScript("window.scrollBy(0,-10000)", "");
-//        Thread.sleep(2000);
-//        TemplatePageObj.ClickClassificationBox();
-//        Thread.sleep(2000);
-//        TemplatePageObj.SelectClassification();
-//        Thread.sleep(2000);
-//        TemplatePageObj.EnterFieldName();
-//        Thread.sleep(2000);
-//        TemplatePageObj.ClickValidation();
-//        Thread.sleep(2000);
-//        TemplatePageObj.SelectValidation();
-//        Thread.sleep(2000);
-//        TemplatePageObj.ClickMap();
-//        Thread.sleep(2000);
-//        TemplatePageObj.ClickCancelTraining();
-//        Thread.sleep(4000);
-//    }
-//    @Test(priority = 8)
-//    public void delete_created_template_single_page() throws Exception {
-//        //TC 20.8 Delete created template (Single Page).
-//        TemplatePageObj.ClickOnSearchTemplate(ReadProps.readAttr("TNameUnique"));
-//        Thread.sleep(2000);
-//        TemplatePageObj.ClickOnExpandTemplate();
-//        Thread.sleep(2000);
-//        TemplatePageObj.DeleteTemplate();
-//        Thread.sleep(2000);
-//        TemplatePageObj.ConfirmDeleteTemplate();
-//        Thread.sleep(3000);
-//    }
+    @Test(priority = 3)
+    public void create_template_with_already_exists_name_no_chosen_file() throws Exception {
+        //TC 20.3 - Create Template with Already Exists Name and Chosen file
+        TemplatePageObj.ClickOnUploadTemplateBtn();
+        Thread.sleep(2000);
+        TemplatePageObj.ClickOnTemplateName(ReadProps.readAttr("TExistName"));
+        Thread.sleep(2000);
+        WebElement upload_file = driver.findElement(By.xpath("//body/app-root[1]/div[1]/app-landing[1]/div[1]/div[1]/main[1]/div[1]/app-new-training[1]/div[3]/div[1]/div[1]/div[2]/div[1]/form[1]/div[1]/input[1]"));
+        upload_file.sendKeys("C:\\Users\\kishor_ghadge\\IdeaProjects\\Template1.jpg");
+        Thread.sleep(3000);
+        TemplatePageObj.ClickCreateTemplate();
+        Thread.sleep(5000);
+        TemplatePageObj.ClickCancelCreateTemplate();
+        Thread.sleep(3000);
+    }
+    @Test(priority = 4)
+    public void create_template_with_valid_name_chosen_file() throws Exception {
+        //TC 20.4 - Create template with Valid Name and chosen file.
+        TemplatePageObj.ClickOnUploadTemplateBtn();
+        Thread.sleep(2000);
+        TemplatePageObj.ClickOnTemplateName(ReadProps.readAttr("TNameUnique"));
+        Thread.sleep(2000);
+WebElement upload_file = driver.findElement(By.xpath("//body/app-root[1]/div[1]/app-landing[1]/div[1]/div[1]/main[1]/div[1]/app-new-training[1]/div[3]/div[1]/div[1]/div[2]/div[1]/form[1]/div[1]/input[1]"));
+
+            upload_file.sendKeys("C:\\Users\\kishor_ghadge\\IdeaProjects\\Template1.jpg");
+        Thread.sleep(2000);
+        TemplatePageObj.ClickCreateTemplate();
+        Thread.sleep(6000);
+        TemplatePageObj.ClickCancel();
+        Thread.sleep(5000);
+    }
+    @Test(priority = 5)
+    public void search_created_template() throws Exception {
+        //TC 20.5 Search Created template.
+        TemplatePageObj.ClickOnSearchTemplate(ReadProps.readAttr("TNameUnique"));
+        Thread.sleep(2000);
+        TemplatePageObj.ClickOnExpandTemplate();
+        Thread.sleep(2000);
+        TemplatePageObj.ClickOnTemplateDataInfo();
+        Thread.sleep(4000);
+    }
+    @Test(priority = 6)
+    public void zoom_in() throws Exception {
+        //TC 20.6 Zoom IN.
+        TemplatePageObj.ClickOnZoomIn();
+        Thread.sleep(2000);
+        TemplatePageObj.ClickOnZoomIn();
+        Thread.sleep(2000);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,10000)", "");
+        Thread.sleep(2000);
+    }
+    @Test(priority = 7)
+    public void zoom_out() throws Exception {
+        //TC 20.7 Zoom OUT.
+        TemplatePageObj.ClickOnZoomOut();
+        Thread.sleep(2000);
+        TemplatePageObj.ClickOnZoomOut();
+        Thread.sleep(2000);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,-10000)", "");
+        Thread.sleep(2000);
+        TemplatePageObj.ClickClassificationBox();
+        Thread.sleep(2000);
+        TemplatePageObj.SelectClassification();
+        Thread.sleep(2000);
+        TemplatePageObj.EnterFieldName();
+        Thread.sleep(2000);
+        TemplatePageObj.ClickValidation();
+        Thread.sleep(2000);
+        TemplatePageObj.SelectValidation();
+        Thread.sleep(2000);
+        TemplatePageObj.ClickMap();
+        Thread.sleep(2000);
+        TemplatePageObj.ClickCancelTraining();
+        Thread.sleep(4000);
+    }
+    @Test(priority = 8)
+    public void delete_created_template_single_page() throws Exception {
+        //TC 20.8 Delete created template (Single Page).
+        TemplatePageObj.ClickOnSearchTemplate(ReadProps.readAttr("TNameUnique"));
+        Thread.sleep(2000);
+        TemplatePageObj.ClickOnExpandTemplate();
+        Thread.sleep(2000);
+        TemplatePageObj.DeleteTemplate();
+        Thread.sleep(2000);
+        TemplatePageObj.ConfirmDeleteTemplate();
+        Thread.sleep(3000);
+    }
     @Test(priority = 9)
     public void create_template_with_more_pages() throws Exception {
         //TC 20.9 Create Template with more pages.
